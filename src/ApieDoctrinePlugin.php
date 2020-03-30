51bb33db272272dd49f367d0ae7096aadf0b3215
Pieter Jordaan <pieter.jordaan@way2web.nl>
2020-03-30 07:30:01 +0200

    Added DoctrineTypeExtractor to Apie

--- src/ApieDoctrinePlugin.php
+++ src/ApieDoctrinePlugin.php
@@ -5,11 +5,13 @@ namespace W2w\Lib\ApieDoctrinePlugin;
 
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\PropertyInfo\DoctrineExtractor;
 use W2w\Lib\Apie\Interfaces\ApiResourceFactoryInterface;
 use W2w\Lib\Apie\PluginInterfaces\ApiResourceFactoryProviderInterface;
+use W2w\Lib\Apie\PluginInterfaces\PropertyInfoExtractorProviderInterface;
 use W2w\Lib\ApieDoctrinePlugin\ResourceFactories\DoctrineDataLayerFactory;
 
-class ApieDoctrinePlugin implements ApiResourceFactoryProviderInterface
+class ApieDoctrinePlugin implements ApiResourceFactoryProviderInterface, PropertyInfoExtractorProviderInterface
 {
     /**
      * @var EntityManagerInterface
@@ -22,7 +24,6 @@ class ApieDoctrinePlugin implements ApiResourceFactoryProviderInterface
     private $additionalEntityManagers;
 
     /**
-     * ApieDoctrinePlugin constructor.
      * @param EntityManagerInterface $entityManager
      * @param EntityManagerInterface[] $additionalEntityManagers
      */
@@ -37,5 +38,59 @@ class ApieDoctrinePlugin implements ApiResourceFactoryProviderInterface
     public function getApiResourceFactory(): ApiResourceFactoryInterface
     {
         return new DoctrineDataLayerFactory($this->entityManager, $this->additionalEntityManagers);
+    }
+
+    /**
+     * @return DoctrineExtractor[]
+     */
+    private function createDoctrineExtractors(): array
+    {
+        $list = [
+            new DoctrineExtractor($this->entityManager)
+        ];
+        foreach ($this->additionalEntityManagers as $entityManager) {
+            $list[] = new DoctrineExtractor($entityManager);
+        }
+        return $list;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getListExtractors(): array
+    {
+        return $this->createDoctrineExtractors();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getTypeExtractors(): array
+    {
+        return $this->createDoctrineExtractors();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDescriptionExtractors(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAccessExtractors(): array
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInitializableExtractors(): array
+    {
+        return [];
     }
 }
